032f8e9d36fe20a38db34ae089ea26b615b596dd
Chanunchita <peachlyhoneyy@gmail.com>
2026-08-29 10:39:46 +0700

    chore: make run_etl.bat portable with relative paths and system python

--- run_etl.bat
+++ run_etl.bat
@@ -1,4 +1,7 @@
 @echo off
-cd /d "C:\Users\MuNe\IBM_challenge\august_challenge\IBM_august_challenge"
-"C:\Users\MuNe\AppData\Local\Programs\Python\Python314\python.exe" etl_pipeline.py > pipeline_output.txt 2>&1
+
+cd /d "%~dp0"
+
+python etl_pipeline.py > pipeline_output.txt 2>&1
+
 type pipeline_output.txt
